1441ee19090dece6bfbc93263bcd74d620d44c3d
pautib <pau.torresibravo@igt.com>
2025-02-09 20:19:20 +0100

    Added style for linked text

--- src/dapp/components/WalletList.tsx
+++ src/dapp/components/WalletList.tsx
@@ -11,9 +11,9 @@ export const WalletList = () => {
       <div className="providers">
         {
           Object.keys(wallets).length > 0 ? Object.values(wallets).map((provider: EIP6963ProviderDetail) => (
-            <button key={provider.info.uuid} onClick={() => connectWallet(provider.info.rdns)} >
-              <img src={provider.info.icon} alt={provider.info.name} />
-              <div>{provider.info.name}</div>
+            <button key={ provider.info.uuid } onClick={() => connectWallet(provider.info.rdns)} >
+              <img src={ provider.info.icon } alt={ provider.info.name } />
+              <div>{ provider.info.name }</div>
             </button>
           )) :
             <div>
